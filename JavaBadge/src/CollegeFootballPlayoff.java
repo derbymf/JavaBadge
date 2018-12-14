e57248acd335378781761287ede995f0c2de05f1
derbymf <43820528+derbymf@users.noreply.github.com>
2018-12-13 19:30:42 -0500

    Add files via upload
    Update comments

--- JavaBadge/src/CollegeFootballPlayoff.java
+++ JavaBadge/src/CollegeFootballPlayoff.java
@@ -12,17 +12,18 @@ public class CollegeFootballPlayoff {
         playoffTeams.add("University of Alabama");
         playoffTeams.add("Clemson University");
         playoffTeams.add("Notre Dame University");
-        //
+        //print the arraylist
         System.out.println("The college football playoff consists of " + playoffTeams.size() + " " +
                 "teams. Those teams are: ");
 
         for (String team: playoffTeams){
             System.out.println(team);
         }
+        //create a new hash map for the scheduled games
         HashMap<String,String> scheduledGames = new HashMap<String, String>();
         scheduledGames.put("Oklahoma and Alabama", "December 29");
         scheduledGames.put("Clemson and Notre Dame", "December 29");
-
+        //loop through all scheduled games and print out who plays and when
         for(String game: scheduledGames.keySet()){
             System.out.println(game + " will play " + scheduledGames.get(game));
         }
